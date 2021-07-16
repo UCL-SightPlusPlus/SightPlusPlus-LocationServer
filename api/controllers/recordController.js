a90548b2900634d7f2f0660d2e40696d2f68ebfc
Lydia Tsami <lydia.tsami@gmail.com>
2021-07-16 10:50:56 +0300

    Revert "Revert "Fix code style"" (#10)

--- api/controllers/recordController.js
+++ api/controllers/recordController.js
@@ -1,23 +1,25 @@
 'use strict';
 
-var mongoose = require('mongoose'),
-  Record = mongoose.model('Records');
+const mongoose = require('mongoose');
+const Record = mongoose.model('Records');
 
 
 exports.list_all_records = function(req, res) {
-    Record.find({}, function(err, task) {
-      if (err)
-        res.send(err);
-      res.json(task);
-    });
+  Record.find({}, function(err, task) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(task);
+  });
 };
 
 exports.create_a_record = function(req, res) {
-    var new_record = new Record(req.body);
-    new_record.save(function(err, record) {
-      if (err)
-        res.send(err);
-      res.json(record);
-    });
+  const newRecord = new Record(req.body);
+  newRecord.save(function(err, record) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(record);
+  });
 };
 
--- api/routes/recordRoute.js
+++ api/routes/recordRoute.js
@@ -1,9 +1,8 @@
 'use strict';
 module.exports = function(app) {
-  var controller = require('../controllers/recordController');
+  const controller = require('../controllers/recordController');
 
   app.route('/records')
-    .get(controller.list_all_records)
-    .post(controller.create_a_record);
-
+      .get(controller.list_all_records)
+      .post(controller.create_a_record);
 };
