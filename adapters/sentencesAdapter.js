db339a66d3a13d8b1eac48d39c187f765344ceb9
Lydia Tsami <lydia.tsami@gmail.com>
2021-07-30 17:36:03 +0300

    Added work of feature/UpdatedGetSentenceByBeacon

--- adapters/sentencesAdapter.js
+++ adapters/sentencesAdapter.js
@@ -9,59 +9,54 @@ exports.automaticMessage = async function(beaconId, lastFloor) {
     const beacon = updater.deviceTable.find(device => device._id == beaconId);
     // console.log(beacon);
     if (beacon) {
-      if (lastFloor) {
-        const sentences = [];
-        // 2 cases: on the same floor or on a different floor
-        if (beacon.floor == lastFloor) {
-          // Return the cameras in the same room as beacon
-          console.log('On last floor');
-          const deviceInLocation = updater.deviceTable.filter(device => {
-            return device.floor == beacon.floor && (device.deviceLocation == beacon.deviceLocation) && (device.deviceType == 'camera');
-          });
-          sentences.push(`You are now on the ${beacon.deviceLocation} area. `);
-          // get latest record of each device
-          Promise.all(deviceInLocation.map(async (device) => {
+      const sentences = [];
+      // 2 cases: on the same floor or on a different floor
+      if (lastFloor && (beacon.floor == lastFloor)) {
+        // Return the cameras in the same room as beacon
+        console.log('On last floor');
+        const deviceInLocation = updater.deviceTable.filter(device => {
+          return device.floor == beacon.floor && (device.deviceLocation == beacon.deviceLocation) && (device.deviceType == 'camera');
+        });
+        sentences.push(`You are now on the ${beacon.deviceLocation} area. `);
+        // get latest record of each device
+        Promise.all(deviceInLocation.map(async (device) => {
+          await Record.findOne({deviceId: device._id}, (err, record) => {
+            if (err) {
+              resolve({'status' :400, 'message': err});
+            } else {
+              sentences.push(createSentenceUsingRecord(record));
+            }
+          }).sort('-timestamp');
+        })).then(() => {
+          const response = {'status': 200, 'message': {'floor': beacon.floor, 'sentence': sentences.join('')}};
+          resolve(response);
+        }).catch((err) => {
+          resolve({'status': 400, 'message': err});
+        });
+      } else {
+        // if on a new floor or last floor is not passed(when user first enter a building), return the records of entire floor
+        console.log('Not on last floor');
+        const deviceOnFloor = updater.deviceTable.filter(device => device.floor == beacon.floor && (device.deviceType == 'camera'));
+        const locations = [...new Set(deviceOnFloor.map(device => device.deviceLocation))];
+        let sentence = `You are now one the ${ordinalSuffixOf(beacon.floor)} floor. On this floor you can find the ${locations.join(' area , the ')} area. `;
+        sentence = sentence.substring(0, sentence.lastIndexOf(',')) + `and` + sentence.substring(sentence.lastIndexOf(',')+1,sentence.length);
+        sentences.push(sentence);
+        locations.map(loc => {
+          let deviceInLocation = deviceOnFloor.filter(device => device.deviceLocation == loc);
+          Promise.all(deviceInLocation.map(async device => {
             await Record.findOne({deviceId: device._id}, (err, record) => {
-              if (err) {
-                resolve({'status' :400, 'message': err});
-              } else {
+              if(record != null){
+                sentences.push(`In the ${loc} area `);
                 sentences.push(createSentenceUsingRecord(record));
               }
-            }).sort('-timestamp');
+            });
           })).then(() => {
-            const response = {'status': 200, 'message': {'floor': beacon.floor, 'sentence': sentences.join('')}};
-            console.log(response);
-            resolve(response);
+            const response = {'floor': beacon.floor, 'sentence': sentences.join('')};
+            resolve( {'status': 200, 'message': response});
           }).catch((err) => {
             resolve({'status': 400, 'message': err});
           });
-        } else {
-          console.log('Not on last floor');
-          const deviceOnFloor = updater.deviceTable.filter(device => device.floor == beacon.floor && (device.deviceType == 'camera'));
-          const locations = [...new Set(deviceOnFloor.map(device => device.deviceLocation))];
-          let sentence = `You are now one the ${ordinalSuffixOf(beacon.floor)} floor. On this floor you can find the ${locations.join(' area , the ')} area. `;
-          sentence = sentence.substring(0, sentence.lastIndexOf(',')) + `and` + sentence.substring(sentence.lastIndexOf(',')+1,sentence.length);
-          sentences.push(sentence);
-          locations.map(loc => {
-            let deviceInLocation = deviceOnFloor.filter(device => device.deviceLocation == loc);
-            Promise.all(deviceInLocation.map(async device => {
-              await Record.findOne({deviceId: device._id}, (err, record) => {
-                if(record != null){
-                  sentences.push(`In the ${loc} area `);
-                  sentences.push(createSentenceUsingRecord(record));
-                }
-              });
-            })).then(() => {
-              const response = {'floor': beacon.floor, 'sentence': sentences.join('')};
-              resolve( {'status': 200, 'message': response});
-            }).catch((err) => {
-              resolve({'status': 400, 'message': err});
-            });
-          });
-        }
-      } else {
-        const errResponse = {'message': 'Last floor is not passed'};
-        resolve({'status': 400, 'message': errResponse});
+        });
       }
     } else {
       const errResponse = {'message': 'Beacon not found'};
