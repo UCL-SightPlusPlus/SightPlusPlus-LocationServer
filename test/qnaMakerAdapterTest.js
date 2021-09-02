99d1827741ed8f0ace68097ebbc12b00c6dcc7a6
Lydia Tsami <lydia.tsami@gmail.com>
2021-09-02 20:12:56 +0300

    Mocked call to QnA service on tests

--- test/qnaMakerAdapterTest.js
+++ test/qnaMakerAdapterTest.js
@@ -1,5 +1,6 @@
 const qnaAdapter = require('../adapters/qnaMakerAdapter');
 const chai = require('chai');
+const nock = require('nock');
 
 describe('QnA Maker Adapter', () => {
   let env;
@@ -11,11 +12,36 @@ describe('QnA Maker Adapter', () => {
 
     it('Should Generate an answer', (done) => {
       const question = 'What time do you open?';
+      nock(`${process.env.KB_HOST}`)
+          .post(`/qnamaker/knowledgebases/${process.env.KB_ID}/generateAnswer`)
+          .reply(200, {
+            'answers': [
+              {
+                'questions': [
+                  'What time do you open?',
+                  'What time do you close?',
+                  'What\'s the opening hours?',
+                ],
+                'answer': 'The location is open everyday from 9am to 6pm',
+                'score': 95.0,
+                'id': 1,
+                'source': 'QnA Maker Sample FAQ.xlsx',
+                'isDocumentText': false,
+                'metadata': [],
+                'context': {
+                  'isContextOnly': false,
+                  'prompts': [],
+                },
+              },
+            ],
+            'activeLearningEnabled': false,
+          });
 
       qnaAdapter.generateAnswer(question).then((sentence) => {
         sentence.should.be.a('object');
         sentence.should.have.property('answers');
         sentence.answers[0].should.have.property('answer').eq('The location is open everyday from 9am to 6pm');
+        nock.cleanAll();
         done();
       });
     }).timeout(4000);
