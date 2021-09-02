0c467e8f8ab3b58804d8aa218500219be8639176
Lydia Tsami <lydia.tsami@gmail.com>
2021-09-02 20:20:49 +0300

    Test update (#30)
    * Added more Unit tests for the Device API
    
    * Updated tests
    
    * Updated tests
    
    * Updated Github script
    
    * Mocked call to QnA service on tests

--- test/sentenceAdapterTest.js
+++ test/sentenceAdapterTest.js
@@ -57,8 +57,9 @@ describe('Sentence Adapter', () => {
       expect(sentence).to.equal('3 seats are available. ');
     });
   });
-  describe('Create create same floor sentences', () => {
-    it('Create a no information about a queue sentence', () => {
+
+  describe('Create sentences when a user changes floors', () => {
+    it('Should create a sentence of a floor that has 3 rooms', () => {
       const beacon = {
         '_id': '2',
         'deviceType': 'BLE',
@@ -68,22 +69,44 @@ describe('Sentence Adapter', () => {
         'floor': 0,
         'maxOccupancy': 60,
       };
+      const locations = ['Reception', 'ER', 'Cardiology'];
+      const sentence = sentenceAdapter.createDiffFloorSentencesFromRecords(beacon, locations);
+      expect(sentence).to.equal('You are now on the ground floor. On this floor you can find the Reception area, the ER area and the Cardiology area. ');
+    });
+  });
+  describe('Create same floor sentences', () => {
+    const beacon = {
+      '_id': '2',
+      'deviceType': 'BLE',
+      'deviceLocation': 'Reception',
+      'site': 'GOSH DRIVE',
+      'isIndoor': true,
+      'floor': 0,
+      'maxOccupancy': 60,
+    };
+    it('Create a no information about a queue sentence', () => {
       const sentence = sentenceAdapter.createSameFloorSentencesFromRecords(beacon, [], 1);
       expect(sentence).to.equal('You are now at the Reception area. There is no information about a queue in this area.');
     });
 
     it('Create a no information about a seating space sentence', () => {
-      const beacon = {
-        '_id': '2',
-        'deviceType': 'BLE',
-        'deviceLocation': 'Reception',
-        'site': 'GOSH DRIVE',
-        'isIndoor': true,
-        'floor': 0,
-        'maxOccupancy': 60,
-      };
       const sentence = sentenceAdapter.createSameFloorSentencesFromRecords(beacon, [], 2);
       expect(sentence).to.equal('You are now at the Reception area. There is no information about a seating space in this area.');
     });
+    it('Create an undefined event sentence', () => {
+      const record = {
+        '_id': '610a9359a624de104090c26c',
+        'timestamp': '2021-08-04T13:17:13.677Z',
+        'deviceId': '996',
+        'recordType': 3,
+        'event': 'Test event',
+      };
+      const sentence = sentenceAdapter.createSentence(record);
+      expect(sentence).to.equal('Current event Test event.');
+    });
+  });
+  it('Create undefined sentence', () => {
+    const sentence = sentenceAdapter.undefinedSentence();
+    expect(sentence).to.equal('I\'m sorry, I do not know the answer to that question.');
   });
 });
